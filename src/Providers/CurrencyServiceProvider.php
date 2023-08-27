fc75f2cc426001039c73cdef1cd837c2b12299df
Aimon Islam <110348549+aimonext@users.noreply.github.com>
2023-08-28 00:01:32 +0600

    wip

---
+++ src/Providers/CurrencyServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class CurrencyServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
