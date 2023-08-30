08747a3925c351077cacb1fc9ceaa65e035055c4
Aimon Islam <110348549+aimonext@users.noreply.github.com>
2023-08-30 23:31:46 +0600

    Add currency instance as global function

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,10 @@
 <?php
+
+use Wowpack\LaravelCurrency\CurrencyBase;
+
+if (!function_exists("currency")) {
+    function currency(): CurrencyBase
+    {
+        return app(CurrencyBase::class);
+    }
+}
