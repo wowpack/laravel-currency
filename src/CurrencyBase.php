745eb00dcddd348370b8c0b6d4a27de91d172e5b
Aimon Islam <110348549+aimonext@users.noreply.github.com>
2023-08-30 23:52:40 +0600

    Add currency conversion functionality

--- src/CurrencyBase.php
+++ src/CurrencyBase.php
@@ -2,6 +2,17 @@
 
 namespace Wowpack\LaravelCurrency;
 
+use Wowpack\LaravelCurrency\Contracts\Convertible;
+use Wowpack\LaravelCurrency\Models\Currency;
+
 class CurrencyBase
 {
+    public function __construct()
+    {
+    }
+
+    public function convert(Currency $from, Currency $to): Convertible
+    {
+        return new Converter($from, $to);
+    }
 }
--- src/Support/Facades/Currency.php
+++ src/Support/Facades/Currency.php
@@ -5,6 +5,9 @@ namespace Wowpack\LaravelCurrency\Support\Facades;
 use Illuminate\Support\Facades\Facade;
 use Wowpack\LaravelCurrency\CurrencyBase;
 
+/**
+ * @method \Wowpack\LaravelCurrency\Contracts\Convertible convert(\Wowpack\LaravelCurrency\Models\Currency $from, \Wowpack\LaravelCurrency\Models\Currency $to)
+ */
 class Currency extends Facade
 {
     protected static function getFacadeAccessor(): string
