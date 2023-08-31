7cb57eb23c66e24ff7bc3a1b81498bb25e05a0d1
Aimon Islam <110348549+aimonext@users.noreply.github.com>
2023-08-31 08:19:44 +0600

    Remove optional public keywords from abstract class

--- src/Contracts/Calculable.php
+++ src/Contracts/Calculable.php
@@ -6,21 +6,21 @@ use Wowpack\LaravelCurrency\Models\Currency;
 
 abstract class Calculable
 {
-    abstract public function __construct(Currency $currency);
+    abstract function __construct(Currency $currency);
 
     abstract protected function calculate(): static;
 
-    abstract public function input(int|float|null $value): static;
+    abstract function input(int|float|null $value): static;
 
-    abstract public function amount(int|float|null $value): static;
+    abstract function amount(int|float|null $value): static;
 
-    abstract public function getValue(): int|float;
+    abstract function getValue(): int|float;
 
-    abstract public function getAmount(): int|float;
+    abstract function getAmount(): int|float;
 
-    abstract public function getResult(): array;
+    abstract function getResult(): array;
 
-    abstract public function toArray(): array;
+    abstract function toArray(): array;
 
-    abstract public function __serialize(): string;
+    abstract function __serialize(): string;
 }
