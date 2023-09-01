40de801a1b277ccc14999f6bc9f25cc490a6e76e
Aimon Islam <110348549+aimonext@users.noreply.github.com>
2023-09-01 19:27:37 +0600

    Added database migrations publishes

--- src/Providers/CurrencyServiceProvider.php
+++ src/Providers/CurrencyServiceProvider.php
@@ -9,6 +9,13 @@ use Wowpack\LaravelCurrency\Support\UserCurrency;
 
 class CurrencyServiceProvider extends ServiceProvider
 {
+    private function offerPublishes()
+    {
+        $this->publishes([
+            __DIR__ . "/../../database/migrations/create_laravel_currency_tables.php" => database_path("migrations/" . date('Y_m_d_His') . "_create_laravel_currency_tables.php"),
+        ]);
+    }
+
     public function register(): void
     {
         $this->app->bind(Base::class, fn () => new CurrencyBase);
@@ -18,5 +25,6 @@ class CurrencyServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if ($this->app->runningInConsole() && function_exists("config_path") && function_exists("database_path")) $this->offerPublishes();
     }
 }
