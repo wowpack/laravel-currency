70b00835d6ca1cbd3c23c645b51261fcbf7b8ff0
Aimon Islam <110348549+aimonext@users.noreply.github.com>
2023-09-02 06:54:39 +0600

    Remove inner User keyword

--- src/Support/Traits/WithUserCurrency.php
+++ src/Support/Traits/WithUserCurrency.php
@@ -19,7 +19,7 @@ trait WithUserCurrency
         return $this->morphToMany(Currency::class, "user", "user_has_currencies", "user_id", "currency_id");
     }
 
-    public function setUserCurrency(Currency $currency)
+    public function setCurrency(Currency $currency)
     {
         $this->removeUserCurrency($currency);
 
@@ -28,14 +28,14 @@ trait WithUserCurrency
         return $this;
     }
 
-    public function removeUserCurrency(Currency|null $currency)
+    public function removeCurrency(Currency|null $currency)
     {
         if (isset($currency)) return $this->currencies()->detach($currency);
 
         return $this->currencies()->detach();
     }
 
-    public function getUserCurrency(): Currency
+    public function getCurrency(): Currency
     {
         return $this->currencies()->first() ?? Currency::first();
     }
