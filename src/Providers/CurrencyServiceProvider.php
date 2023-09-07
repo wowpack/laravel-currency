f77d396df50742bc17351f8c714a5e56a398061d
Aimon Islam <110348549+aimonext@users.noreply.github.com>
2023-09-07 20:50:28 +0600

    Remove unused dependency

--- src/Providers/CurrencyServiceProvider.php
+++ src/Providers/CurrencyServiceProvider.php
@@ -5,7 +5,6 @@ namespace Wowpack\LaravelCurrency\Providers;
 use Illuminate\Support\ServiceProvider;
 use Wowpack\LaravelCurrency\Base;
 use Wowpack\LaravelCurrency\CurrencyBase;
-use Wowpack\LaravelCurrency\Support\UserCurrency;
 
 class CurrencyServiceProvider extends ServiceProvider
 {
@@ -23,8 +22,6 @@ class CurrencyServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(Base::class, fn () => new CurrencyBase);
-
-        $this->app->singleton(UserCurrency::class, fn () => new UserCurrency);
     }
 
     public function boot(): void
